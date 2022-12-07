0a4e9b78dd7792eb1a009c2cf325a4f67ead07b2
Derek White <derekwhite.m@gmail.com>
2022-12-06 23:13:47 -0500

    commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const navLinks = document.querySelectorAll('.nav__item');
+const nav = document.querySelector('nav');
+const navHeight = nav.offsetHeight;
+
 const highlightNav = () => {
-    const navLinks = document.querySelectorAll('.nav__item');
-    const nav = document.querySelector('nav');
-    const navHeight = nav.offsetHeight;
     navLinks.forEach(navLink => {
-        const section = document.querySelector(navLink.attributes.href.value);
-        if (window.scrollY + navHeight + (window.innerHeight / 2 - navHeight) >= section.offsetTop) {
+        const section = document.getElementById(navLink.dataset.section_id);
+        if (window.scrollY + navHeight + ((window.innerHeight / 2.5) - navHeight) >= section.offsetTop) {
             const activeLink = document.querySelector('.nav__item.active');
             if (activeLink) { activeLink.classList.remove('active'); }
             navLink.classList.add('active');
@@ -12,5 +13,17 @@ const highlightNav = () => {
     });
 };
 
+const scrollToSection = sectionId => {
+    const section = document.getElementById(sectionId);
+    window.scroll({
+        top: section.offsetTop - navHeight
+    });
+};
+
 window.addEventListener('load', highlightNav);
 window.addEventListener('scroll', highlightNav);
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        scrollToSection(navLink.dataset.section_id)
+    });
+});
